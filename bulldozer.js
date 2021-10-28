b2d220953ed237bfc8a24355ee32b5bf1f34772e
Geahad Haymor <79179978+GSRHackZ@users.noreply.github.com>
2021-10-28 17:29:28 -0400

    Update bulldozer.js

--- bulldozer.js
+++ bulldozer.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         New York Times Bulldozer
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Bypasses New York Times paywall & removes all ads.
 // @author       GSRHackZ
 // @match        https://www.nytimes.com/*
@@ -11,35 +11,39 @@
 
 
 function unlockArticle(){
-    setTimeout(()=>{
-        window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' })
-        let story = document.getElementById("story"),
-            all = document.body.innerHTML,
-            text = story.innerText,
-            html = story.innerHTML,
-            done = false,
-            paywall,
-            alerted = false,
-            unlock = setInterval(()=>{
-                if(document.getElementById("gateway-content")!==undefined){
-                    paywall = document.getElementById("gateway-content");
-                    if(!done){
-                        document.body.innerHTML = all;
-                        done=true;
-                        removeTrash();
-                        showPics();
+    if(location.href.replaceAll("/","")!==location.origin.replaceAll("/","")){
+        setTimeout(()=>{
+            window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' })
+            let story,
+                all = document.body.innerHTML,
+                done = false,
+                paywall,
+                alerted = false,
+                unlock = setInterval(()=>{
+                    if(document.getElementById("gateway-content")!==undefined){
+                        paywall = document.getElementById("gateway-content");
+                        if(!done){
+                            document.body.innerHTML = all;
+                            done=true;
+                            removeTrash();
+                        }
+                        else{
+                            clearInterval(unlock);
+                            showPics();
+                            window.scrollTo({ top: 0, behavior: 'smooth' });
+                            setTimeout(()=>{
+                                alert("News article unlocked and ads removed successfully! 🔓");
+                            },500)
+                        }
                     }
-                    else{
-                        clearInterval(unlock);
-                        window.scrollTo({ top: 0, behavior: 'smooth' });
-                        setTimeout(()=>{
-                            alert("News article unlocked successfully! 🔓");
-                        },500)
-                    }
-                }
-            },100);
-    },500)
-    // More news pages coming soon....
+                },100);
+        },500)
+        // More news pages coming soon....
+    }
+}
+
+String.prototype.replaceAll = function(str1, str2, ignore){
+    return this.replace(new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g,"\\$&"),(ignore?"gi":"g")),(typeof(str2)=="string")?str2.replace(/\$/g,"$$$$"):str2);
 }
 
 function removeTrash(){
@@ -49,23 +53,32 @@ function removeTrash(){
     setInterval(()=>{
         let ads = document.getElementsByClassName("ads");
         let others = document.getElementsByClassName("ad");
+        let special = document.getElementsByClassName("css-11cg26");
         for(let i=0;i<ads.length;i++){
             ads[0].remove();
         }
         for(let i=0;i<others.length;i++){
             others[0].remove();
         }
+        for(let i=0;i<special.length;i++){
+            if(special[0].id.includes("ad-")){
+                special[0].remove();
+            }
+        }
     },100)
-    bottom.remove();
-    top.remove();
-    sub.remove();
+    bottom.remove()
+    top.remove()
+    sub.remove()
 }
 
 function showPics(){
-    let pics = document.getElementsByTagName("picture");
-    for(let i=0;i<pics.length;i++){
-        pics[i].style.opacity="100";
-    }
+    setInterval(()=>{
+        let pics = document.getElementsByTagName("picture");
+        for(let i=0;i<pics.length;i++){
+            pics[i].style.opacity="100";
+        }
+    },1000)
 }
+
 
 unlockArticle();
